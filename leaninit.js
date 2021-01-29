99108823d135cd3dcb5d3836811652343cb741be
JupiterJun <hbbdmjz@163.com>
2021-01-29 10:08:20 +0800

    wangong

---
+++ leaninit.js
@@ -0,0 +1,15 @@
+AV.init({
+    appId: "43xARai57goiu18VJjAIHmsn-gzGzoHsz",
+    appKey: "upyc9ei1WHzdlUkwmiCd2wgB",
+    serverURL: "https://43xarai5.lc-cn-n1-shared.com"
+});
+
+localStorage.setItem('debug', 'leancloud*');
+
+function log(message){
+    if (true) {
+        console.log(message);
+    }
+}
+
+var $ = mdui.$;
